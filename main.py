7b75a02c8efd96d7b1efd5441aa4b2fc8fc14bfd
Dbas8452 <124307364+Dbas8452@users.noreply.github.com>
2023-03-09 15:09:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print(...)
+print("Domenic_Basden")
+
+def name_and_grade(last_name, first_intial, grade):
+
+  # Format sentence below
+  sentence = "my name is {} {} {}".format(last_name,first_intial,grade)
+  # Don't forget to return the formatted sentence
+  return sentence
+print(name_and_grade("Basden", "D", 11))
+
+def consecutive_numbers(n):
+
+  number_list =[]
+
+  for i in range(1,n):
+    number_list.append(i)
+
+  return number_list
+print(consecutive_numbers(30))
+
+def factors(n):
+
+  factor_list = []
+
+  for i in range(1, n):
+    
+    if n % i == 0:
+      factor_list.append(i)
+      
+
+  return factor_list
+
+print(factors(50))
+
+def Grade_Midterm
+   
+    
+print
